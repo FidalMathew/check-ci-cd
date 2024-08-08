e518b9a83fc1b184a68244caac9752e50d150124
Fidal <fidal15perfect@gmail.com>
2024-08-08 19:56:02 +0530

    update

---
+++ contract/TestName.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Test {
+    function getName(address name) public pure returns (address) {
+        return name;
+    }
+}
