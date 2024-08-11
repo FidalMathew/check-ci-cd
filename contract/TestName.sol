6e8615493f7ba81265861c488fe65ea2b156acb7
Fidal Mathew <84982038+FidalMathew@users.noreply.github.com>
2024-08-11 15:12:18 +0530

    Update TestName.sol

--- contract/TestName.sol
+++ contract/TestName.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Test {
+contract TestName {
     function getName(address name) public pure returns (address) {
         return name;
     }
