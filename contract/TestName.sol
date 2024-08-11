7125249a26340aaa42b5db44ecdfebde12da461f
Fidal Mathew <84982038+FidalMathew@users.noreply.github.com>
2024-08-11 19:19:54 +0530

    Update TestName.sol

--- contract/TestName.sol
+++ contract/TestName.sol
@@ -2,12 +2,22 @@
 pragma solidity ^0.8.13;
 
 contract TestName {
-    function getName(address name) public pure returns (address) {
-        return name;
+
+    // State variable to store the name
+    string public name;
+
+    // Function to return the input address (this function doesn't interact with the state)
+    function getName(address _name) public pure returns (address) {
+        return _name;
     }
 
+    // Function to set a new name
     function setName(string memory newName) public {
-        // Do something
+        name = newName; // Store the new name in the state variable
     }
 
+    // Function to retrieve the current name
+    function retrieveName() public view returns (string memory) {
+        return name; // Return the stored name
+    }
 }
